50beee86fd8ed5e172b60ba4791f55c9c5bb8b62
David Camacho J <dcjpriv@gmail.com>
2020-12-22 08:43:29 -0400

    Initial Commit

---
+++ src/cmps/button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const {{ .initial }} = ({ children, ...props }) => {
+  return <button>{children}</button>;
+};
---
+++ src/cmps/logo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Logo = ({ children, ...props }) => {
+  return <div>Logo</div>;
+};
